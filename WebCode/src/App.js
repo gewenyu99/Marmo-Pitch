9ac87b5fb95343d489f211007e1d2b3c26809b11
Jonathan Sun <Jspsun@gmail.com>
2018-06-28 14:49:03 -0400

    initial

---
+++ WebCode/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import DirectionButton from './components/DirectionButton';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DirectionButton/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ WebCode/src/components/DirectionButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DirectionButton.css';
+
+class DirectionButton extends Component {
+  render() {
+    return (
+      <div className="DirectionButton">
+        button
+      </div>
+    );
+  }
+}
+
+export default DirectionButton;
